c760b9d4cf52e3f616eef7d361a3411e38b122d8
Attila Bruder <bruderattila@gmail.com>
2021-09-30 21:40:05 +0200

    Vezetett rész vége.

--- NgOtthonbazarClient/src/app/app.component.ts
+++ NgOtthonbazarClient/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Advertisement } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) { 
+
+  }
+  advertisements!: Advertisement[];
   title = 'otthonbazar';
+  navBarCollapsed = true;
+
+  ngOnInit() {
+    this.http.get<Advertisement[]>('/api/advertisements').subscribe(resp =>
+    this.advertisements = resp
+    );
+    }
 }
